b0620970065e341debd924a513c9ce346febac59
Mahammad Prabej Hussain <contact.prabej@gmail.com>
2026-08-28 16:48:11 +0545

    feat: add response routers

--- src/modules/response/routes/responseRoutes.ts
+++ src/modules/response/routes/responseRoutes.ts
@@ -1,30 +1,51 @@
 import { Router } from "express";
+
+import authMiddleware from "../../../middlewares/authMiddleware";
+import roleMiddleware from "../../../middlewares/roleMiddleware";
+import { sanitize } from "../../../middlewares/sanitize";
+
 import responseController from "../controllers/responseController";
+
+import {
+  createResponseSchema,
+  updateResponseSchema,
+} from "../validation/responseValidation";
 
 const router = Router();
 
+router.post(
+  "/:surveyId",
+  sanitize(createResponseSchema),
+  responseController.create
+);
+
+
 router.get(
   "/",
+  authMiddleware,
+  roleMiddleware("ADMIN"),
   responseController.getAll
 );
 
 router.get(
   "/:id",
+  authMiddleware,
+  roleMiddleware("ADMIN"),
   responseController.getById
-);
-
-router.post(
-  "/",
-  responseController.create
 );
 
 router.patch(
   "/:id",
+  authMiddleware,
+  roleMiddleware("ADMIN"),
+  sanitize(updateResponseSchema),
   responseController.update
 );
 
 router.delete(
   "/:id",
+  authMiddleware,
+  roleMiddleware("ADMIN"),
   responseController.remove
 );
 
