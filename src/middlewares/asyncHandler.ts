e9bdf09dd6bfcde47ce64b9274e9863bc6a2099a
Mahammad Prabej Hussain <contact.prabej@gmail.com>
2026-08-28 16:09:30 +0545

    feat(util): add asyncHandler function

---
+++ src/middlewares/asyncHandler.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, RequestHandler, Response } from "express";
+
+const asyncHandler = (
+  handler: (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => Promise<any>
+): RequestHandler => {
+  return (req, res, next) => {
+    Promise.resolve(handler(req, res, next)).catch(next);
+  };
+};
+
+export default asyncHandler;
