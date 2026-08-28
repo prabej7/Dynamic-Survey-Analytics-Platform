5523fca6a20b588fa9f367b75c25a86b777d30fd
Mahammad Prabej Hussain <contact.prabej@gmail.com>
2026-08-28 15:05:57 +0545

    refactor: import format

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import dotenv from "dotenv";
 dotenv.config({ path: "../.env" });
 
-import express, { NextFunction, Response, Request } from "express";
 import cors from "cors";
+import express, { NextFunction, Request, Response } from "express";
 
 const app = express();
 
