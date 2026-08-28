e7120be1c211d2d90d53ce9ee469c9491fe087fc
Mahammad Prabej Hussain <contact.prabej@gmail.com>
2026-08-28 16:48:28 +0545

    feat: add response routes to registry routes

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import authRoutes from "./auth/routes/authRoutes";
 import surveyRoutes from "./survey/routes/surveyRoutes";
+import responseRoutes from "./response/routes/responseRoutes";
 
 const router = Router();
 router.use("/v1/auth", authRoutes);
 router.use("/v1/surveys", surveyRoutes);
+router.use("/v1/responses", responseRoutes);
 
 export default router;
