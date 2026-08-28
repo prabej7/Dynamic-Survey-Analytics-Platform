ee51bee802121f7aee68820c848e2d2524bc8510
Mahammad Prabej Hussain <contact.prabej@gmail.com>
2026-08-28 15:05:45 +0545

    feat: add survey validation

---
+++ src/modules/survey/validation/surveyValidation.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+export const createSurveySchema = z.object({
+  // TODO: Define validation schema
+});
+
+export const updateSurveySchema = z.object({
+  // TODO: Define validation schema
+});
