2adf9780cf751bd1789c74332008b4c69cca76ff
Mahammad Prabej Hussain <contact.prabej@gmail.com>
2026-08-28 20:07:20 +0545

    feat: add dashboard router to registry

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import authRoutes from "./auth/routes/authRoutes";
+import dashboardRoutes from "./dashboard/routes/dashboardRoutes";
 import responseRoutes from "./response/routes/responseRoutes";
 import surveyRoutes from "./survey/routes/surveyRoutes";
-
 const router = Router();
 router.use("/v1/auth", authRoutes);
 router.use("/v1/surveys", surveyRoutes);
 router.use("/v1/responses", responseRoutes);
+router.use("/v1/dashboard", dashboardRoutes);
 
 export default router;
