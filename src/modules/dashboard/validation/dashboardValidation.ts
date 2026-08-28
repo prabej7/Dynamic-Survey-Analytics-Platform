0e0a76a68a75b7c99fd8214400a9352b4688a833
Mahammad Prabej Hussain <contact.prabej@gmail.com>
2026-08-28 20:06:30 +0545

    feat: add dashboard validation

---
+++ src/modules/dashboard/validation/dashboardValidation.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+export const createDashboardSchema = z.object({
+  // TODO: Define validation schema
+});
+
+export const updateDashboardSchema = z.object({
+  // TODO: Define validation schema
+});
